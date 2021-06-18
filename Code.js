d3b9c8c9d4bc02cf293ec85a67e4a091ff208aad
ektabhagat06 <75603768+ektabhagat06@users.noreply.github.com>
2021-06-18 18:13:02 +0530

    Treasure Hunt
    Treasure Hunt is probably the most popular of printable party games for kids of the moment! The treasure hunt is a group activity in which children must find a treasure previously hidden by parents or organizers.

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("lightblue")
+    text("Hint: Stores all information !!", 100,270)
+
+}
---
+++ sketch.js
@@ -0,0 +1,32 @@
+var bg,bg2,form,system,code,security;
+var score=0;
+
+function preload() {
+  bg = loadImage("aladdin_cave.jpg");
+  bg2 = loadImage("treasure.jpg")
+}
+
+function setup() {
+  createCanvas(1000,500);
+  system = new System()
+  security = new Security()
+}
+
+function draw() {
+  background(bg);
+  clues();
+  security.display();
+  textSize(20);
+  fill("white");
+  text("Score: " + score, 450, 50);
+
+  if(score === 3) {
+    clear()
+    background(bg2)
+    fill("black")
+    textSize(40);
+    text("TREASURE UNLOCKED",250, 200);
+  }
+
+  drawSprites()
+}
